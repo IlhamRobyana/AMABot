16e5481f262c343f1c9c6046395a1b46c3f3585c
Ilham Robyana Sofyan <ilhamrobyanasofyan@gmail.com>
2018-11-23 14:17:34 +0700

    Update AMABot.py

--- AMABot.py
+++ AMABot.py
@@ -5,41 +5,40 @@ import asyncio
 import aiohttp
 import json
 from discord import Game
-from discord.ext.commands import Bot
+from discord.ext import commands
 
 BOT_PREFIX = ("AMA.")
 TOKEN = 'NTE0NjM1NjQ5NDg4MjU3MDI1.DtZfHg.FMchMpxkqczKwm9o3tSX-T38PqU'
 
-client = Bot(command_prefix=BOT_PREFIX)
+bot = commands.Bot(command_prefix=BOT_PREFIX)
 
-@client.command(name='hello',pass_context=True)
-async def hello(context):
-    await client.say("Hello " + context.message.author.mention)
+@bot.command()
+async def hello(ctx):
+    await ctx.send("Hello " + ctx.message.author.mention)
 
-@client.command(name='roll',pass_context=True)
-async def roll(context):
+@bot.command()
+async def roll(ctx):
     dice = random.randint(1,101)
-    await client.say(context.message.author.mention + " rolled " + str(dice))
+    await ctx.send(ctx.message.author.mention + " rolled " + str(dice))
 
-@client.command()
-async def padoru():
-    await client.say("https://media.discordapp.net/attachments/460643534685470730/506770854252642304/spindoru_fast.gif")
+@bot.command()
+async def padoru(ctx):
+    await ctx.send("https://media.discordapp.net/attachments/460643534685470730/506770854252642304/spindoru_fast.gif")
 
-@client.event
+@bot.event
 async def on_ready():
     print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
+    print(bot.user.name)
+    print(bot.user.id)
     print('------')
-    await client.change_presence(game=Game(name="with you"))
 
 async def list_servers():
-    await client.wait_until_ready()
-    while not client.is_closed:
+    await bot.wait_until_ready()
+    while not bot.is_closed:
         print("Current servers:")
-        for server in client.servers:
+        for server in bot.servers:
             print(server.name)
         await asyncio.sleep(600)
 
 # client.loop.create_task(list_servers())
-client.run(TOKEN)
+bot.run(TOKEN)
